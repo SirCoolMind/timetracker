1c5d6e422d2bb3dedcb419936feba9209744b2e7
SirCoolMind <90669475+hafizunijaya@users.noreply.github.com>
2023-01-12 08:51:35 +0800

    [Webpack] Added yajra datatable

--- resources/assets/core/custom.js
+++ resources/assets/core/custom.js
@@ -11,5 +11,31 @@ try {
     window.Swal = require('sweetalert2');
     window.flatpickr = require('flatpickr');
 
+    /*-- Datatables Jquery with all extension --*/
+    window.JSZip = require('jszip');
+    require('datatables.net-bs5');
+    require('datatables.net-buttons/js/dataTables.buttons');
+    require('datatables.net-buttons/js/buttons.flash');
+    require('datatables.net-buttons/js/buttons.html5');
+    require('datatables.net-buttons/js/buttons.print');
+    require('datatables.net-buttons/js/buttons.colVis');
+    require('datatables.net-autofill');
+    require('datatables.net-colreorder');
+    require('datatables.net-datetime');
+    require('datatables.net-fixedcolumns');
+    require('datatables.net-fixedheader');
+    require('datatables.net-keytable');
+    require('datatables.net-rowgroup');
+    require('datatables.net-rowreorder');
+    require('datatables.net-scroller');
+    require('datatables.net-searchbuilder');
+    require('datatables.net-searchpanes');
+    require('datatables.net-select');
+    require('datatables.net-staterestore');
+    window.pdfMake = require('pdfmake/build/pdfmake');
+    window.pdfFonts = require('pdfmake/build/vfs_fonts');
+    pdfMake.vfs = pdfFonts.pdfMake.vfs;
+    /*-- End Datatables Jquery with all extension --*/
+
 } catch (e) { }
 
